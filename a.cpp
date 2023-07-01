86045fa167a0627a04d0826277111d1b3064bcc9
Piotr Kasprzyk <ciri@ciri.pl>
2023-07-01 09:37:48 +0200

    Add Diamond class

--- a.cpp
+++ a.cpp
@@ -1,6 +1,15 @@
 using namespace std;
 #include <iostream>
 
+class Diamond
+{
+    public:
+    int year;
+    int month;
+    int day;
+    string summary_text;
+};
+
 int main(void)
 {
     cout << "Startowanie" << endl;
